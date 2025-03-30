861c8c3422bf078ef4824ac4d3cbea7f0d47bb19
PollitoFrito7 <jdelrod04@gmail.com>
2025-03-30 01:12:43 +0100

    new sprites
    smoke and fog reworked. smoker in progress

--- P.Tactics/src/PTactics/view/GUI/Icons.java
+++ P.Tactics/src/PTactics/view/GUI/Icons.java
@@ -20,7 +20,7 @@ public interface Icons {
 		
 		public static interface SmokerIcons {
 			public final ImageIcon TROOP_FACING_UP = new ImageIcon("Icons/LightTroop_up.png");
-			public final ImageIcon TROOP_FACING_DOWN = new ImageIcon("Icons/LightTroop_down.png"); 
+			public final ImageIcon TROOP_FACING_DOWN = new ImageIcon("Icons/inprogressSmoker.png"); 
 			public final ImageIcon TROOP_FACING_LEFT = new ImageIcon("Icons/LightTroop_left.png");
 			public final ImageIcon TROOP_FACING_RIGHT = new ImageIcon("Icons/LightTroop_right.png");
 		}
@@ -36,10 +36,10 @@ public interface Icons {
 	public static interface otherIcons {
 		public final ImageIcon WALL = new ImageIcon("Icons/inprogressWall.png");
 		//public final ImageIcon WALL = new ImageIcon("Icons/Wall.png");
-		public final ImageIcon FOG = new ImageIcon("Icons/inprogressFog.png");
+		public final ImageIcon FOG = new ImageIcon("Icons/FoggedFloor.png");
 		//public final ImageIcon FOG = new ImageIcon("Icons/certainly_fog.png");
 		public final ImageIcon FLOOR = new ImageIcon("Icons/inprogressFloor.png");
 		//public final ImageIcon FLOOR = new ImageIcon("Icons/Floor.png");
-		public final ImageIcon SMOKE= new ImageIcon("Icons/smoke.png");
+		public final ImageIcon SMOKE= new ImageIcon("Icons/SmokedFloor.png");
 	}
 }
